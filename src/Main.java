fe5ffa5f0eb3a36f2ea49714abd939e43ffb2b80
Ankit Saini <asaini@dasnuve.com>
2023-06-14 17:24:08 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter first number - ");
+            var a = sc.nextBigDecimal();
+            System.out.print("Enter second number - ");
+            var b = sc.nextBigDecimal();
+            sc.nextLine();
+            System.out.print("Enter sign for calculation (+,-,/,*)- ");
+            String c = sc.nextLine();
+            switch (c) {
+                case "+":
+                    System.out.println(a.add(b));
+                    break;
+                case "-":
+                    System.out.println(a.subtract(b));
+                    break;
+                case "*":
+                    System.out.println(a.multiply(b));
+                    break;
+                case "/":
+                    System.out.println(a.divide(b));
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
