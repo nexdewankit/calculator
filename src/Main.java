5d0aa869a81ed020a7eba5bc868e278dee9e4dc9
Ankit Saini <asaini@dasnuve.com>
2023-06-14 18:25:22 +0530

    Calculator code merge

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter first number - ");
+            var a = sc.nextBigDecimal();
+            System.out.print("Enter second number - ");
+            var b = sc.nextBigDecimal();
+            sc.nextLine();
+            System.out.print("Enter sign for calculation (+,-,/,*)- ");
+            String c = sc.nextLine();
+            switch (c) {
+                case "+":
+                    System.out.println(a.add(b));
+                    break;
+                case "-":
+                    System.out.println(a.subtract(b));
+                    break;
+                case "*":
+                    System.out.println(a.multiply(b));
+                    break;
+                case "/":
+                    System.out.println(a.divide(b));
+                    break;
+            }
+        } catch (NoSuchElementException | IllegalStateException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
